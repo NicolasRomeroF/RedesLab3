e85e514401c01fc7367e0ffba7cc7b62e9c3b93c
Nico <nicolas.romero.f@usach.cl>
2018-07-25 11:04:05 -0400

    am listo

--- lab3.py
+++ lab3.py
@@ -30,7 +30,7 @@ def abrirArchivo():
 def interpolacion(data,rate):
     tiempo = np.linspace(0,len(data)/rate, num=len(data))
     interp = interp1d(tiempo,data)
-    tiempo2 = np.linspace(0,len(data)/rate,len(data)*8)
+    tiempo2 = np.linspace(0,len(data)/rate,len(data)*4)
     y = interp(tiempo2)
     return y
 
@@ -38,9 +38,10 @@ def modulacionAM(data,rate,mod_index):
     senal_interp = interpolacion(data,rate)
     largo=len(senal_interp)
     tiempo= np.linspace(0,len(data)/(rate), num=largo)
-    frecuencuencia_portadora=20000
+    frecuencuencia_portadora=400
     portadora = np.cos(2*np.pi*frecuencuencia_portadora*tiempo)*mod_index
     y = senal_interp * portadora
+    graficar("Señal modulada","Tiempo","Amplitud",tiempo,y)
     
     return y
 
@@ -53,7 +54,7 @@ def demodulacionAM(signal,time,freq_demod):
 def modulacionFM(data,rate,mod_index):
     timp = len(data)/rate
     t=np.linspace(0,timp,len(data))
-    frecuencuencia_portadora=3000
+    frecuencuencia_portadora=400
     portadora=2*np.pi*frecuencuencia_portadora*t*mod_index
     y = np.cos(portadora+data)
 
@@ -69,7 +70,7 @@ def graficar(title,xlabel,ylabel,X,Y):
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.plot(X, Y)
+    plt.plot(X, Y, "-")
     print("Mostrando grafico")
     plt.show()
 
@@ -80,7 +81,7 @@ def lowpass_filter(data,rate):
     fircoef1_low = signal.firwin(numtaps,cutoff_hz/nyq_rate, window = "hamming")
     filtered_x = signal.lfilter(fircoef1_low,1.0,data)
     return filtered_x
-
+'''
 data,rate = abrirArchivo()
 timp = len(data)/rate
 t=np.linspace(0,timp,len(data))
@@ -92,10 +93,17 @@ new_rate=rate*8
 
 timp = len(data)/rate
 time=np.linspace(0,timp,len(data)*8)
+'''
+rate=2500
+time = np.linspace(0,1,num=rate)
+data = np.cos(2*np.pi*100*time)
+new_rate=rate*4
 
+graficar("Señal original","Tiempo","Amplitud",time,data)
 
+time_resample = np.linspace(0,1,num=new_rate)
 senalModulada = modulacionAM(data,rate,1)
-senalDemodulada = demodulacionAM(senalModulada,time,20000)
+senalDemodulada = demodulacionAM(senalModulada,time_resample,400)
 senal_demod_filtrada = lowpass_filter(senalDemodulada,new_rate)
 data_resample=interpolacion(data,rate)
 
